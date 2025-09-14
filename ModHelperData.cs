c9e51f91ebed49298dda3fb2a8109d8a44abb9e2
arnonymer66 <arnonymer649@gmail.com>
2025-09-14 13:55:39 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace MonkeyMoney;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "50.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "MonkeyMoney";
 
     public const string Description = "Press (E) to open menu, Give you're self the amount of Monkey Money You wanna have";
