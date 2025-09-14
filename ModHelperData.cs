ef87309fa04695cb6b7f407013e7c798fe0958db
arnonymer66 <arnonymer649@gmail.com>
2025-09-14 12:02:02 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,6 +8,6 @@ public static class ModHelperData
 
     public const string Description = "An empty mod";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "arnonymer66"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "MonkeyMoney"; // TODO add your repo name here, also in the download url in README.md
 }
